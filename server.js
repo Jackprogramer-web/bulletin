a8d6a74e21c80e68d08fae3151ad820ed6f57167
jword <didehdrb21@naver.com>
2023-06-04 16:58:47 +0900

    게시판 페이지를 위한 id parameter구현

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const express = require("express");
 const app = express();
 const port = 4001;
 
+// bodyParser 연결
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 // mariaDB연결
 const mariadb = require("mysql");
+
+//cors
+const cors = require("cors");
+app.use(cors());
 
 const connection = mariadb.createConnection({
 	host: "localhost",
@@ -12,6 +20,15 @@ const connection = mariadb.createConnection({
 	user: "root",
 	password: "1234",
 	database: "bulletin",
+});
+
+// 서버 연결 확인
+connection.connect((err) => {
+	if (err) {
+		console.error("error", err);
+	} else {
+		console.log("connect!!");
+	}
 });
 
 app.get("/", (req, res) => {
@@ -25,8 +42,40 @@ app.get("/board", (req, res) => {
 			res.status(500).json({ error: "An error occurred" });
 			return;
 		}
-		res.json(results);
+
+		res.send(results);
 	});
+});
+
+app.get("/board/:id", (req, res) => {
+	connection.query("select * from 게시글", function (error, results, fields) {
+		if (error) {
+			console.error("Error executing query:", error);
+			res.status(500).json({ error: "An error occurred" });
+			return;
+		}
+
+		let id = req.params.id;
+		let idx = id - 1;
+		res.send(results[idx]);
+	});
+});
+
+app.post("/api/posts", (req, res) => {
+	const { title, text } = req.body;
+
+	connection.query(
+		"INSERT INTO 게시글 (글제목, 글내용, 작성일) VALUES (?, ?, NOW())",
+		[title, text],
+		function (error, results, fields) {
+			if (error) {
+				console.error("에러가 발생했음", error);
+				res.status(500).json({ error: "An error occurred" });
+				return;
+			}
+			res.json(results);
+		}
+	);
 });
 
 app.listen(port);
