4b54ae356d0234aca5dae0687fe87ba8ebe5624a
jword <didehdrb21@naver.com>
2023-06-09 14:19:34 +0900

    댓글 기능 추가

--- server.js
+++ server.js
@@ -35,9 +35,22 @@ app.get("/", (req, res) => {
 	res.send("hello ");
 });
 
-// 글 목록 확인
+// GET
+// 전체 글 데이터
 app.get("/board", (req, res) => {
 	connection.query("select * from 게시글", function (error, results, fields) {
+		if (error) {
+			console.error("Error executing query:", error);
+			res.status(500).json({ error: "An error occurred" });
+			return;
+		}
+
+		res.send(results);
+	});
+});
+// 전체 댓글 데이터
+app.get("/comment", (req, res) => {
+	connection.query("select * from 댓글", function (error, results, fields) {
 		if (error) {
 			console.error("Error executing query:", error);
 			res.status(500).json({ error: "An error occurred" });
@@ -57,25 +70,44 @@ app.get("/board/:id", (req, res) => {
 			return;
 		}
 
-		console.log(results);
 		let id = parseInt(req.params.id);
+
 		const targetBoard = results.find((board) => board["글번호"] === id);
 
 		if (targetBoard) {
 			res.send(targetBoard); // 찾은 객체를 전체로 반환하거나 원하는 작업 수행
 		} else {
-			console.log("글번호가 1인 객체를 찾을 수 없습니다.");
+			res.send(`글번호가 ${id}인 객체를 찾을 수 없습니다.`);
 		}
 	});
 });
 
-// 글쓰기
-app.post("/api/posts", (req, res) => {
-	const { title, text } = req.body;
+// 해당 글에 대한 댓글 불러오기
+app.get("/comment/:id", (req, res) => {
+	let id = parseInt(req.params.id);
+	connection.query(
+		"select * from 댓글 where 게시글번호 = ?",
+		[id],
+		function (error, results, fields) {
+			if (error) {
+				console.error("Error executing query:", error);
+				res.status(500).json({ error: "An error occurred" });
+				return;
+			} else {
+				res.send(results);
+			}
+		}
+	);
+});
+
+// POST
+// 게시글
+app.post("/api/posts/board", (req, res) => {
+	const { title, text, writer } = req.body;
 
 	connection.query(
-		"INSERT INTO 게시글 (글제목, 글내용, 작성일) VALUES (?, ?, NOW())",
-		[title, text],
+		"INSERT INTO 게시글 (글제목, 글내용, 작성자, 작성일) VALUES (?, ?, ?, NOW())",
+		[title, text, writer],
 		function (error, results, fields) {
 			if (error) {
 				console.error("에러가 발생했음", error);
@@ -83,7 +115,24 @@ app.post("/api/posts", (req, res) => {
 				return;
 			}
 			res.json(results);
-			res.send("hello");
+		}
+	);
+});
+
+// 댓글
+app.post("/api/posts/comment", (req, res) => {
+	const { idx, user, comment } = req.body;
+
+	connection.query(
+		"INSERT INTO 댓글 (게시글번호, 작성자, 내용, 작성일) VALUES (?, ?, ?, NOW())",
+		[idx, user, comment],
+		function (error, results, fields) {
+			if (error) {
+				console.error("에러가 발생했음", error);
+				res.status(500).json({ error: "An error occurred" });
+				return;
+			}
+			res.json(results);
 		}
 	);
 });
@@ -91,6 +140,7 @@ app.post("/api/posts", (req, res) => {
 // 글삭제하기
 app.delete("/api/delete/:id", (req, res) => {
 	const id = req.params.id;
+
 	connection.query(
 		"DELETE FROM 게시글 WHERE 글번호 = (?)",
 		[id],
